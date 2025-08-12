a897841de8aca97bc6e7ed8048bf5a078fe61ba1
akomolafetemitopejoseph <134941960+akomolafetemitopejoseph@users.noreply.github.com>
2025-08-12 14:40:01 -0700

    Added navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./Component/Navbar";
+import Footer from "./Component/Footer";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div></div>
-  )
-}
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { LuSearch } from "react-icons/lu";
+import { FaShoppingCart } from "react-icons/fa";
+import { IoPeopleSharp } from "react-icons/io5";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-white shadow-md">
+      <div className="mx-auto p-3 md:px-16 lg:px-2 flex justify-between items-center">
+        <div className="text-lg font-bold">
+          <Link to="/">
+            <h1>e-Shop</h1>
+          </Link>
+        </div>
+
+        <div className="relative flex-1 mx-4">
+          <form action="">
+            <input
+              type="text"
+              placeholder="Search Product"
+              className="w-full border py-2 px-4"
+            />
+            <LuSearch className="absolute top-3 right-3 text-red-500" />
+          </form>
+        </div>
+        <div className="flex items-center space-x-4">
+          <Link to="/cart">
+            <FaShoppingCart className="text-lg" />
+          </Link>
+          <button className="hidden md:block capitalize">
+            login | register
+          </button>
+          <button className="block md:hidden">
+            <IoPeopleSharp />
+          </button>
+        </div>
+      </div>
+      <div className="flex items-center justify-center space-x-10 py-4 text-sm font-black capitalize">
+        <Link to="/" className="hover:underline">
+          {" "}
+          home
+        </Link>
+        <Link to="/" className="hover:underline">
+          {" "}
+          shop
+        </Link>
+        <Link to="/" className="hover:underline">
+          {" "}
+          contact
+        </Link>
+        <Link to="/" className="hover:underline">
+          {" "}
+          About
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
